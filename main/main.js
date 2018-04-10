ad322b471cfc7f74cd8316a5265031e8c5565810
orange <759550141@qq.com>
2018-04-10 10:34:17 +0800

    finished

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(distance,wait_time) {
+    let price=0;
+    if(distance<=2){
+      price=6;
+    }else if(distance<=8){
+      price=6+0.8*(distance-2);
+    }else{
+      price=6+0.8*6+0.8*1.5*(distance-8);
+    }
+    if(wait_time!=0){
+      price+=0.25*wait_time;
+    }
+    return Math.round(price);
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,21 @@
-const main = require('../main/main');
+const main = require('../main/main.js');
 
 describe('taxi fee', function () {
     // write your tests here...
+  it('2公里以内计价', function() {
+    var result = main(1, 0);
+    expect(result).toEqual(6);
+  });
+  it('8公里以内计价', function() {
+    var result = main(6, 0);
+    expect(result).toEqual(9);
+  });
+  it('8公里以外计价', function() {
+    var result = main(10, 0);
+    expect(result).toEqual(13);
+  });
+  it('增加等待时间', function() {
+    var result = main(10, 10);
+    expect(result).toEqual(16);
+  });
 });
